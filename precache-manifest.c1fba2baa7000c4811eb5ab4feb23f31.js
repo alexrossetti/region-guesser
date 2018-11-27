261725c2a50e34a45c24696cc74eb69942151d8a
alexrossetti <alexjoakland@gmail.com>
2018-11-27 15:19:29 +0000

    Updates

---
+++ precache-manifest.c1fba2baa7000c4811eb5ab4feb23f31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75937cd98e20c9af4cb5",
+    "url": "/region-guesser/static/css/main.7dba12e6.chunk.css"
+  },
+  {
+    "revision": "75937cd98e20c9af4cb5",
+    "url": "/region-guesser/static/js/main.75937cd9.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/region-guesser/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "dfb59486e38231984de9",
+    "url": "/region-guesser/static/js/runtime~main.dfb59486.js"
+  },
+  {
+    "revision": "a2732bce9f730436f1391285a0b75124",
+    "url": "/region-guesser/index.html"
+  }
+];
